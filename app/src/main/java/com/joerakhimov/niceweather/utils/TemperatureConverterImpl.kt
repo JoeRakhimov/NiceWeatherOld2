5a2b0cb0fe9b654bbcebf7a2e9145599005c737b
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-01-30 13:13:13 +0500

    asynctask

--- app/src/main/java/com/joerakhimov/niceweather/utils/TemperatureConverterImpl.kt
+++ app/src/main/java/com/joerakhimov/niceweather/utils/TemperatureConverterImpl.kt
@@ -1,19 +1,33 @@
 package com.joerakhimov.niceweather.utils
 
-import android.os.Handler
-import android.os.Looper
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import javax.inject.Inject
 
 class TemperatureConverterImpl @Inject constructor(): TemperatureConverter {
 
     override fun fromCelsiusToFahrenheit(celsius: Double, callback: (Double) -> Unit) {
-        val handler = Handler(Looper.getMainLooper())
-        Thread(Runnable {
-            val fahrenheit = celsius * 1.8 + 32
-            handler.post(Runnable {
-                callback(fahrenheit)
-            })
-        }).start()
+
+        // Example with Handler
+//        val handler = Handler(Looper.getMainLooper())
+//        Thread(Runnable {
+//            val fahrenheit = celsius * 1.8 + 32
+//            handler.post(Runnable {
+//                callback(fahrenheit)
+//            })
+//        }).start()
+
+        // Example with AsyncTask
+        object: AsyncTask<Double, Nothing, Double>(){
+            override fun doInBackground(vararg celcius: Double?): Double {
+                return celsius * 1.8 + 32
+            }
+            override fun onPostExecute(result: Double?) {
+                super.onPostExecute(result)
+                result?.let { callback(it) }
+            }
+        }.execute(celsius)
+
     }
 
 }
