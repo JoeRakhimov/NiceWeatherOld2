4d3496a6b5b53aa9289f869ede79b2693413ccab
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-01-30 14:17:37 +0500

    RxJava

--- app/src/main/java/com/joerakhimov/niceweather/utils/TemperatureConverterImpl.kt
+++ app/src/main/java/com/joerakhimov/niceweather/utils/TemperatureConverterImpl.kt
@@ -2,9 +2,13 @@ package com.joerakhimov.niceweather.utils
 
 import android.annotation.SuppressLint
 import android.os.AsyncTask
+import android.util.Log
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
-class TemperatureConverterImpl @Inject constructor(): TemperatureConverter {
+class TemperatureConverterImpl @Inject constructor() : TemperatureConverter {
 
     override fun fromCelsiusToFahrenheit(celsius: Double, callback: (Double) -> Unit) {
 
@@ -18,16 +22,24 @@ class TemperatureConverterImpl @Inject constructor(): TemperatureConverter {
 //        }).start()
 
         // Example with AsyncTask
-        object: AsyncTask<Double, Nothing, Double>(){
-            override fun doInBackground(vararg celcius: Double?): Double {
-                return celsius * 1.8 + 32
-            }
-            override fun onPostExecute(result: Double?) {
-                super.onPostExecute(result)
-                result?.let { callback(it) }
-            }
-        }.execute(celsius)
+//        object: AsyncTask<Double, Nothing, Double>(){
+//            override fun doInBackground(vararg celcius: Double?): Double {
+//                return celsius * 1.8 + 32
+//            }
+//            override fun onPostExecute(result: Double?) {
+//                super.onPostExecute(result)
+//                result?.let { callback(it) }
+//            }
+//        }.execute(celsius)
 
+        Single.create {
+            val fahrenheit = celsius * 1.8 + 32
+            it.onSuccess(fahrenheit)
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                callback(it)
+            }, { Log.d("WeatherTag", "Something went wrong") })
     }
 
 }
