ebe724c1db16939ab7aaa7eb23a4da49a3211c18
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2023-02-17 16:43:08 +0500

    Custom Suspendable API

---
+++ app/src/main/java/com/joerakhimov/niceweather/coroutines/CoroutineExtensions.kt
@@ -0,0 +1,6 @@
+package com.joerakhimov.niceweather.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlin.coroutines.suspendCoroutine
+
+
--- app/src/main/java/com/joerakhimov/niceweather/forecast/ForecastActivity.kt
+++ app/src/main/java/com/joerakhimov/niceweather/forecast/ForecastActivity.kt
@@ -6,13 +6,29 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.joerakhimov.niceweather.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import javax.inject.Inject
+import kotlin.coroutines.suspendCoroutine
+
+
 
 @AndroidEntryPoint
 class ForecastActivity : AppCompatActivity() {
+
+    suspend fun <T: Any> getValue(provider: () -> T): T =
+        suspendCoroutine { continuation ->
+            continuation.resumeWith(Result.runCatching { provider() })
+        }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun executeBackground(action: suspend () -> Unit){
+        GlobalScope.launch { action() }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun executeMain(action: suspend () -> Unit){
+        GlobalScope.launch(context = Dispatchers.Main) { action() }
+    }
 
     @Inject
     lateinit var api: ForecastApi
@@ -24,12 +40,24 @@ class ForecastActivity : AppCompatActivity() {
     }
 
     private fun getForecast() {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                val forecast = api.getForecast()
-                forecast.daily?.let { showForecast(forecast) }
-            }
+
+//        runBlocking {
+//            withContext(Dispatchers.IO) {
+//                val forecast = api.getForecast()
+//                forecast.daily?.let { showForecast(forecast) }
+//            }
+//        }
+
+//        executeBackground {
+//            val forecast = getValue { api.getForecast() }
+//            executeMain { forecast.daily?.let { showForecast(forecast) } }
+//        }
+
+        executeBackground {
+            val forecast = api.getForecast()
+            executeMain { forecast.daily?.let { showForecast(forecast) } }
         }
+
     }
 
     private fun showForecast(forecast: ForecastResponse) {
